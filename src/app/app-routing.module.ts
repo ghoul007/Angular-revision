cc9e5dbf861893677cbe457dd6d9555eae023aab
ahmed GHOUL <ghoul.ahmed5@gmail.com>
2020-03-05 16:05:26 +0000

    setting up  routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule, Component } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+
+
+
+const approutes: Routes = [
+  { path: '', redirectTo:'/recipes', pathMatch: 'full'},
+  { path: 'recipes', component: RecipesComponent},
+  { path: 'shopping-list', component: ShoppingListComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(approutes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+
+}
