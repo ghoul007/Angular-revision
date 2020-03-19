847808efc34f0e5d8fb45f3bd3f5bd153198f5be
ahmed <ahmed.elghoul@infor.com>
2020-03-19 23:09:48 +0100

    add key in environement

--- src/app/auth/auth.Service.ts
+++ src/app/auth/auth.Service.ts
@@ -1,70 +1,71 @@
-import {Injectable} from '@angular/core';
-import {HttpClient, HttpErrorResponse} from '@angular/common/http';
-import {catchError, tap} from 'rxjs/internal/operators';
-import {BehaviorSubject, throwError} from 'rxjs';
-import {User} from './user.model';
-import {Router} from '@angular/router';
+import { environment } from './../../environments/environment';
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { catchError, tap } from 'rxjs/internal/operators';
+import { BehaviorSubject, throwError } from 'rxjs';
+import { User } from './user.model';
+import { Router } from '@angular/router';
 
 export interface AuthResponse {
-  idToken	:string
-  email	:string
-  refreshToken	:string
-  expiresIn	:string
-  localId	:string
-  registered ?:	boolean
+  idToken: string
+  email: string
+  refreshToken: string
+  expiresIn: string
+  localId: string
+  registered?: boolean
 }
 
-@Injectable({ providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class AuthServise {
 
   user = new BehaviorSubject<User>(null);
   private expTimer;
 
-  constructor(private http:HttpClient,
-              private router:Router) {}
+  constructor(private http: HttpClient,
+    private router: Router) { }
 
-  signup(userData){
+  signup(userData) {
     return this.http
       .post<AuthResponse>(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDx7iNUi6tfwdfNFFbQhu16Ej_rgflTluE',
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + environment.firebaseAPIKey,
         {
-            email: userData.email,
-            password: userData.password,
-            returnSecureToken:true
-      }).pipe(catchError(
-        this.handleError),
-        tap((resData)=> {
-          this.handleUser(resData.email, resData.localId,resData.expiresIn,resData.expiresIn);
-        }
-      ))
+          email: userData.email,
+          password: userData.password,
+          returnSecureToken: true
+        }).pipe(catchError(
+          this.handleError),
+          tap((resData) => {
+            this.handleUser(resData.email, resData.localId, resData.expiresIn, resData.expiresIn);
+          }
+          ))
   }
 
-  login(userData){
+  login(userData) {
 
-    return this.http.post<AuthResponse>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDx7iNUi6tfwdfNFFbQhu16Ej_rgflTluE',
+    return this.http.post<AuthResponse>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + environment.firebaseAPIKey,
       {
         email: userData.email,
         password: userData.password,
-        returnSecureToken:true
+        returnSecureToken: true
       }).pipe(catchError(this.handleError),
-      tap((resData)=> {
-        this.handleUser(resData.email, resData.localId,resData.idToken,resData.expiresIn);
-      }))
+        tap((resData) => {
+          this.handleUser(resData.email, resData.localId, resData.idToken, resData.expiresIn);
+        }))
   }
 
-  autoLogin(){
+  autoLogin() {
     const userData = JSON.parse(localStorage.getItem('userData'));
-    if(!userData)
+    if (!userData)
       return;
     const loadedUser = new User(
-      userData.email,userData.id,userData._token,userData._tokenExpirationDate
+      userData.email, userData.id, userData._token, userData._tokenExpirationDate
     )
 
-    if(loadedUser.token){
+    if (loadedUser.token) {
       this.user.next(loadedUser);
 
       let expDuration = new Date(userData._tokenExpirationDate).getTime() - new Date().getTime();
-      console.log('autologin time :',expDuration)
+      console.log('autologin time :', expDuration)
       this.autoLogout(expDuration)
     }
   }
@@ -73,51 +74,51 @@ export class AuthServise {
     this.user.next(null);
     localStorage.removeItem('userData');
     this.router.navigate(['/auth']);
-    if(this.expTimer){
+    if (this.expTimer) {
       clearTimeout(this.expTimer);
     }
     this.expTimer = null;
   }
 
-  autoLogout(expDuration:number){
-    this.expTimer =  setTimeout(() => {
+  autoLogout(expDuration: number) {
+    this.expTimer = setTimeout(() => {
       this.logout();
-    },expDuration)
+    }, expDuration)
   }
 
-  handleUser(email:string,id:string,token:string ,exp:string){
+  handleUser(email: string, id: string, token: string, exp: string) {
     let expDate = new Date(new Date().getTime() + +exp * 1000);
 
     let user = new User(
-      email,id,token,expDate
+      email, id, token, expDate
     );
     this.user.next(user);
-    localStorage.setItem('userData',JSON.stringify(user));
-    console.log('user loginlou',+exp * 1000)
+    localStorage.setItem('userData', JSON.stringify(user));
+    console.log('user loginlou', +exp * 1000)
     this.autoLogout(+exp * 1000)
   }
 
-  handleError(errorRes:HttpErrorResponse){
+  handleError(errorRes: HttpErrorResponse) {
 
     let errorMessage = 'This is a Unknown Error';
 
-    if(!errorRes.error || !errorRes.error.error){
+    if (!errorRes.error || !errorRes.error.error) {
       return throwError(errorMessage)
     }
 
-    switch (errorRes.error.error.message){
+    switch (errorRes.error.error.message) {
       case 'EMAIL_EXISTS':
-        errorMessage='This Email already Exist.'
-            break;
+        errorMessage = 'This Email already Exist.'
+        break;
       case 'EMAIL_NOT_FOUND':
-        errorMessage='This Email is Not Found.'
-            break;
-      case 'INVALID_PASSWORD' :
-        errorMessage='Wrong Password , PLease Try again.'
-            break;
-      case 'USER_DISABLED' :
+        errorMessage = 'This Email is Not Found.'
+        break;
+      case 'INVALID_PASSWORD':
+        errorMessage = 'Wrong Password , PLease Try again.'
+        break;
+      case 'USER_DISABLED':
         errorMessage = 'Premium Member Only';
-            break;
+        break;
     }
     return throwError(errorMessage);
   }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseAPIKey: 'AIzaSyDx7iNUi6tfwdfNFFbQhu16Ej_rgflTluE'
 };
 
 /*
