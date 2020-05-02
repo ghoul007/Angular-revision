04a54211b58ee816d9b36f7c9a0c18f58e4fca67
ahmed <ahmed.elghoul@infor.com>
2020-05-03 00:48:34 +0100

    add store recipes in effects

--- src/app/recipes/recipes-resolver.service.ts
+++ src/app/recipes/recipes-resolver.service.ts
@@ -1,27 +1,53 @@
+import { Actions, ofType } from '@ngrx/effects';
+import * as RecipeActions  from './store/recipe.actions';
+import { Store } from '@ngrx/store';
 import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from "@angular/router";
 import { Recipe } from "./recipe.model";
 import { Injectable } from "@angular/core";
 import { DataStorageService } from "../shared/data-storage.service";
-import { Observable } from "rxjs";
+import { Observable, of } from "rxjs";
 import { RecipeService } from "./recipe.service";
+import { AppState } from '../store/app.reducer';
+import { take, map, switchMap } from 'rxjs/operators';
 
-@Injectable({ providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class RecipeResolverService implements Resolve<Recipe[]>{
-    dataStorageService:DataStorageService;
-    recipeService:RecipeService;
+  dataStorageService: DataStorageService;
+  recipeService: RecipeService;
 
-    constructor(dataStorageService:DataStorageService,recipeService:RecipeService){
-        this.dataStorageService=dataStorageService;
-        this.recipeService=recipeService;
-    }
+  constructor(dataStorageService: DataStorageService,
+    private store: Store<AppState>,
+    private actions$: Actions,
+    recipeService: RecipeService) {
+    this.dataStorageService = dataStorageService;
+    this.recipeService = recipeService;
+  }
 
-    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Recipe[] | Observable<Recipe[]> | Promise<Recipe[]> {
-      const recipes = this.recipeService.getRecipes();
-      if (recipes.length === 0 ) {
-        return this.recipeService.getRecipes();
-      } else {
-        return recipes;
-      }
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot){
+    // const recipes = this.recipeService.getRecipes();
+    // if (recipes.length === 0) {
+    //   return this.recipeService.getRecipes();
+    // } else {
+    //   return recipes;
+    // }
 
-    }
+    return this.store.select("recipes").pipe(
+      take(1),
+      map(recipesState => recipesState.recipes),
+      switchMap(recipes => {
+        if (!recipes.length) {
+          this.store.dispatch(new RecipeActions.FetchRcipes());
+          return this.actions$.pipe(
+            ofType(RecipeActions.SET_RECIPES),
+            take(1)
+          );
+        } else {
+          return of(recipes);
+        }
+      })
+    );
+
+
+
+  }
 }
--- src/app/recipes/store/recipe.effects.ts
+++ src/app/recipes/store/recipe.effects.ts
@@ -1,10 +1,12 @@
-import { map } from 'rxjs/operators';
+import { Store } from '@ngrx/store';
+import { map, withLatestFrom } from 'rxjs/operators';
 import { Recipe } from './../recipe.model';
 import { HttpClient } from '@angular/common/http';
 import { switchMap } from 'rxjs/operators';
 import { Actions, Effect, ofType } from '@ngrx/effects';
 import * as RecipesActions from './recipe.actions'
 import { Injectable } from '@angular/core';
+import { AppState } from 'src/app/store/app.reducer';
 
 @Injectable()
 export class RecipeEffects {
@@ -26,8 +28,21 @@ export class RecipeEffects {
         map(recipes => {
             return new RecipesActions.SetRecipes(recipes);
         })
-    )
+    );
 
 
-    constructor(private action$: Actions, private http: HttpClient) { }
+
+    @Effect({ dispatch: false })
+    storeRecipes = this.action$.pipe(
+        ofType(RecipesActions.STORE_RECIPES),
+        withLatestFrom(this.store.select("recipes")),
+        switchMap(([action, recipesState]) => {
+            return this.http.put(
+                "https://recipe-book-ng-project-6e3d2.firebaseio.com/recipes.json",
+                recipesState.recipes
+            );
+        })
+    );
+
+    constructor(private action$: Actions, private http: HttpClient, private store: Store<AppState>) { }
 }
